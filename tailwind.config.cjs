2286c5a4dd12615802ab4c6c7858d98b49f88185
Jedy Matt Tabasco <jedymatt@gmail.com>
2023-02-17 14:25:39 +0800

    Refactor

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,13 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: "#239a4e",
+				"primary-20": "#C7F0D6",
+				"primary-10": "#d3ffee",
+			},
+		},
 	},
 	plugins: [],
 }
